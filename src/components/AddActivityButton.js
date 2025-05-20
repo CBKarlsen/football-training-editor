e61eed69462eb4d9ed7a8ed0e16298a1f402e41f
Casper Karlsen <casperbkarlsen@gmail.com>
2025-05-20 17:52:27 +0200

    A little change

--- src/components/AddActivityButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AddActivityButton({ onAddDrill }) {
-  return (
-    <button onClick={onAddDrill}>Add Drill</button>
-  );
-}
-
-export default AddActivityButton;
